737211dfa0ea7065b49183403728c742ddcc5866
beraldoLucca <lucca.beraldo@aluno.ifsp.edu.br>
2025-05-13 00:36:55 -0300

    first commit

---
+++ src/main/java/br/com/fiap/fast_food_payment/FastFoodPaymentApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fast_food_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFoodPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFoodPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/fast_food_payment/FastFoodPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fast_food_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
